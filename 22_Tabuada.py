ce5b92ec9044c7d2e283f708b48463982e080661
Romulo Vieira <romulo.vieira777@gmail.com>
2020-04-03 15:52:50 -0300

    The Huxley

---
+++ 22_Tabuada.py
@@ -0,0 +1,16 @@
+num = int(input())
+
+if num >0:
+    print(num,"X 1 =",num*1)
+    print(num,"X 2 =",num*2)
+    print(num,"X 3 =",num*3)
+    print(num,"X 4 =",num*4)
+    print(num,"X 5 =",num*5)
+    print(num,"X 6 =",num*6)
+    print(num,"X 7 =",num*7)
+    print(num,"X 8 =",num*8)
+    print(num,"X 9 =",num*9)
+
+else:
+    print("Digite umm número maior que zero")
+
